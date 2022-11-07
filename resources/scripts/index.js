3f408e4d08fee79a90264559f93d827b706570a1
jheasley02 <heaslejn@mail.uc.edu>
2022-11-07 02:51:31 -0500

    game controller

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,12 +1,14 @@
+const { get } = require("lodash");
+
 // Elements
 const welcomeScreen = document.getElementById(`welcome-screen`);
-const gameScreen = ;
-const startGameButton = ;
-const userName = ;
-const userSelection = ;
-const goButton = ;
-const scoreParagraph = ;
-const gameHistoryParagraph = ;
+const gameScreen = document.getElementById(`game-screen`);
+const startGameButton = document.getElementById(`start-game-button`);
+const userName = document.getElementById(`username`);
+const userSelection = document.getElementById(`user-selection`);
+const goButton = document.getElementById(`go-button`);
+const scoreParagraph = document.getElementById(`score`);
+const gameHistoryParagraph = document.getElementById(`game-history`);
 
 // instantiate the game object from the `RockPaperScissors` class.
 let game;
@@ -21,18 +23,19 @@ function updateScoreTallyUI(){
 
 // updateGameHistoryUI
 function updateGameHistoryUI(){
-
+  const score = scoreParagraph;
 }
 
 // start-game-button EventListener
 startGameButton.addEventListener(`click`, function () {
-  const username = 
+  const username = userName;
   game = new RockPaperScissors(userName);
   // Complete
 });
 
 // go-button EventListener
 goButton.addEventListener(`click`, function () {
+  get userSelection; 
   
 });
 
